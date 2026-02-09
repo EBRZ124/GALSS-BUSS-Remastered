ea4d7e5a323d53e007bd83c9b86afc2cb758724a
EBRZ124 <130497524+EBRZ124@users.noreply.github.com>
2026-02-09 19:58:50 +0200

    Updated time plan

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,25 +1,118 @@
 import { Fragment } from "react/jsx-runtime";
 
 function ListGroup() {
-  const items = ["Riga", "Tallin", "Brussel", "Tokyo"];
-
-  const message = items.length === 0 ? <p>No cities found</p> : null;
-
   return (
     <Fragment>
-      <h1>List</h1>
-      {message}
-      <ul className="list-group">
-        {items.map((item) => (
-          <li
-            className="list-group-item"
-            key={item}
-            onClick={() => console.log("works")}
-          >
-            {item}
-          </li>
-        ))}
-      </ul>
+      <div className="list-group">
+        <a href="#" className="list-group-item list-group-item-action">
+          <div className="d-flex w-100 justify-content-between">
+            <h5 className="mb-1">05:45 - 06:15</h5>
+            <small className="text-body-secondary">30min</small>
+          </div>
+          <small className="text-body-secondary">
+            Maršruts: Salaspils (Skola) - Rīga
+          </small>
+        </a>
+        <a href="#" className="list-group-item list-group-item-action">
+          <div className="d-flex w-100 justify-content-between">
+            <h5 className="mb-1">06:25 - 06:55</h5>
+            <small className="text-body-secondary">30min</small>
+          </div>
+          <small className="text-body-secondary">
+            Maršruts: Salaspils (Skola) - Rīga
+          </small>
+        </a>
+        <a href="#" className="list-group-item list-group-item-action">
+          <div className="d-flex w-100 justify-content-between">
+            <h5 className="mb-1">06:55 - 07:25</h5>
+            <small className="text-body-secondary">30min</small>
+          </div>
+          <small className="text-body-secondary">
+            Maršruts: Salaspils (Skola) - Rīga
+          </small>
+        </a>
+        <a href="#" className="list-group-item list-group-item-action">
+          <div className="d-flex w-100 justify-content-between">
+            <h5 className="mb-1">07:30 - 08:00</h5>
+            <small className="text-body-secondary">30min</small>
+          </div>
+          <small className="text-body-secondary">
+            Maršruts: Salaspils (Skola) - Rīga
+          </small>
+        </a>
+        <a href="#" className="list-group-item list-group-item-action">
+          <div className="d-flex w-100 justify-content-between">
+            <h5 className="mb-1">08:10 - 08:40</h5>
+            <small className="text-body-secondary">30min</small>
+          </div>
+          <small className="text-body-secondary">
+            Maršruts: Salaspils (Skola) - Rīga
+          </small>
+        </a>
+        <a href="#" className="list-group-item list-group-item-action">
+          <div className="d-flex w-100 justify-content-between">
+            <h5 className="mb-1">06:25 - 06:55</h5>
+            <small className="text-body-secondary">30min</small>
+          </div>
+          <small className="text-body-secondary">
+            Maršruts: Salaspils (Skola) - Rīga
+          </small>
+        </a>
+        <a href="#" className="list-group-item list-group-item-action">
+          <div className="d-flex w-100 justify-content-between">
+            <h5 className="mb-1">08:35 - 09:05</h5>
+            <small className="text-body-secondary">30min</small>
+          </div>
+          <small className="text-body-secondary">
+            Maršruts: Salaspils (Skola) - Rīga
+          </small>
+        </a>
+        <a href="#" className="list-group-item list-group-item-action">
+          <div className="d-flex w-100 justify-content-between">
+            <h5 className="mb-1">09:00 - 09:30</h5>
+            <small className="text-body-secondary">30min</small>
+          </div>
+          <small className="text-body-secondary">
+            Maršruts: Salaspils (Skola) - Rīga
+          </small>
+        </a>
+        <a href="#" className="list-group-item list-group-item-action">
+          <div className="d-flex w-100 justify-content-between">
+            <h5 className="mb-1">09:35 - 10:05</h5>
+            <small className="text-body-secondary">30min</small>
+          </div>
+          <small className="text-body-secondary">
+            Maršruts: Salaspils (Skola) - Rīga
+          </small>
+        </a>
+        <a href="#" className="list-group-item list-group-item-action">
+          <div className="d-flex w-100 justify-content-between">
+            <h5 className="mb-1">10:10 - 10:40</h5>
+            <small className="text-body-secondary">30min</small>
+          </div>
+          <small className="text-body-secondary">
+            Maršruts: Salaspils (Skola) - Rīga
+          </small>
+        </a>
+        <a href="#" className="list-group-item list-group-item-action">
+          <div className="d-flex w-100 justify-content-between">
+            <h5 className="mb-1">10:35 - 11:05</h5>
+            <small className="text-body-secondary">30min</small>
+          </div>
+          <small className="text-body-secondary">
+            Maršruts: Salaspils (Skola) - Rīga
+          </small>
+        </a>
+        <a href="#" className="list-group-item list-group-item-action">
+          <div className="d-flex w-100 justify-content-between">
+            <h5 className="mb-1">11:10 - 11:40</h5>
+            <small className="text-body-secondary">30min</small>
+          </div>
+          <small className="text-body-secondary">
+            Maršruts: Salaspils (Skola) - Rīga
+          </small>
+        </a>
+      </div>
     </Fragment>
   );
 }
